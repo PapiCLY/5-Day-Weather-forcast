8801b4f059effbb84c9c87ca4dfa17372374e6f8
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-07 09:11:10 -0400

    updating fetch data

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,16 @@
-
-//connect with the DOM
-let searchInput = document.querySelector('#searchField')
-let searchBtn = document.querySelector('#searchbtn')
-
-//button click to search for city
-searchBtn.addEventListener('click', function(){
-    findCity()
+var apiKey = '8ba90b6b36072ccf5c4c41cbde0e83bb'
+let searchBtn = document.querySelector('#searchBtn').addEventListener('click', ()=>{
+    getWeather()
 })
-
-//function to call API
-function findCity(){
-    let weatherInfo = $('#searchField').val();
-    
-    let apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${weatherInfo}&limit=5&appid=35413ec59e50fae20859778ef25f379f`
-    fetch(apiURL, {method: 'GET'}, 
-    headers: {
-        'X-RapidAPI-Key': '8959ff9965msh4479dc11ddcddc4p1bcd30jsndfe8e2947c47',
-        'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-    }).then(function(){
-        if(response.ok){
-            response.json().then(function(data){
-                let city = data[0].location.name
-                let cityIcon = 
-                let temp = 
-                let wind = 
-                let hum = 
-                let fiveDay = 
-                let fiveIcon = 
-            })
-        }
+let searchField = document.querySelector('#searchField').value
+let url = `api.openweathermap.org/data/2.5/weather?q=${searchField}&appid=${apiKey}`
+getWeather = ()=>{
+    fetch(url)
+    .then(res=> res.json())
+    .then(data=>{
+        console.log(data.data.weather[0].main)
     })
+    .catch(err=>{
+        console.log(`error${err}`)
+    })
+}
