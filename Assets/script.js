dd25399f73780b2fed54ef2f613c721e7429042b
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-11 18:38:38 -0400

    added notes on next steps

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+//Need to pull API data, use local storage to save previous searches, then show a 5 day weather for the searched city
+
 var apiKey = '8ba90b6b36072ccf5c4c41cbde0e83bb'
 let searchBtn = document.querySelector('#searchBtn').addEventListener('click', ()=>{
     getWeather()
@@ -7,7 +9,7 @@ getWeather = ()=>{
 
     let searchField = document.querySelector('#searchField').value
     let url = `api.openweathermap.org/data/2.5/weather?q=${searchField}&appid=${apiKey}`
-    
+
     fetch(url)
     .then(res=> res.json())
     .then(data=>{
