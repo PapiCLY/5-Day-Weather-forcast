ed764ecb328fbd978d442ea08aeb55c3895277a1
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-20 23:43:22 -0400

    added updated HTML tags

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 //Need to pull API data, use local storage to save previous searches, then show a 5 day weather for the searched city
-=======
+// =======
 //Need to find long/lat for coordinates
->>>>>>> b781220b289080b88942c52b7e2ca7e7efe4cf39
+// >>>>>>> b781220b289080b88942c52b7e2ca7e7efe4cf39
 
-var apiKey = '8ba90b6b36072ccf5c4c41cbde0e83bb'
+let apiKey = 'ce6c5ccfc970f8d24550539f01a4e90d'
 let searchBtn = document.querySelector('#searchBtn').addEventListener('click', ()=>{
     getWeather()
 })
@@ -12,12 +12,12 @@ let searchBtn = document.querySelector('#searchBtn').addEventListener('click', (
 getWeather = ()=>{
 
     let searchField = document.querySelector('#searchField').value
-    let url = `api.openweathermap.org/data/2.5/weather?q=${searchField}&appid=${apiKey}`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchField}&cnt=1&appid=${apiKey}&units=imperial`
 
     fetch(url)
     .then(res=> res.json())
     .then(data=>{
-        console.log(data.data.weather[0].main)
+        let icon =  document.querySelector()data.weather[0].icon
     })
     .catch(err=>{
         console.log(`error${err}`)
